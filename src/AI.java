a44a409d109d657aa8040f8a4613d3cf16c91137
catkane-doodles <43806522+catkane-doodles@users.noreply.github.com>
2020-04-21 16:35:24 +0800

    Made Code Sexier

--- src/AI.java
+++ src/AI.java
@@ -1,78 +1,53 @@
-import java.util.ArrayList;
-import java.util.Dictionary;
-import java.util.Enumeration;
-import java.util.Hashtable;
+import java.util.*;
 
 public class AI
 {
-	private double minimax(Board board, String option)
+	private double[] minimax(Board board, boolean Maximising)
 	{
 		int score = board.check();
 		if (score == 1)
-			return -10.0;
+			return new double[]{-10.0, 0, 0};
 		else if (score == 2)
-			return 10.0;
+			return new double[]{10.0, 0, 0};
 
 		int[][] testBoard = board.getBoard();
 		ArrayList<int[]> availablePosition = new ArrayList<int[]>();
 		for (int y = 0; y < 3; y++)
 			for (int x = 0; x < 3; x++)
 				if (testBoard[y][x] == 0)
-					availablePosition.add(new int[] {y, x});
+					availablePosition.add(new int[]{y, x});
 
 		if (score == 0 && availablePosition.isEmpty())
-			return 0.0;
+			return new double[]{0.0, 0, 0};
 
-		if (option.equals("Max"))
+		double[] bestScore = Maximising ? new double[]{Double.NEGATIVE_INFINITY} : new double[]{Double.POSITIVE_INFINITY};
+		int[][] bestMove = {{0, 0}};
+		int option = Maximising ? 2 : 1;
+		availablePosition.forEach(position ->
 		{
-			double bestScore = Double.NEGATIVE_INFINITY;
-			for (int[] position : availablePosition)
+			board.place(position[0], position[1], option, true);
+			double[] results = minimax(board, !Maximising);
+			if (Maximising)
 			{
-				board.place(position[0], position[1], 2, true);
-				bestScore = Double.max(bestScore, minimax(board, "Min"));
-				board.remove(position[0], position[1]);
-			}
-			return bestScore;
-		}
-		if (option.equals("Min"))
-		{
-			double bestScore = Double.POSITIVE_INFINITY;
-			for (int[] position : availablePosition)
+				if (results[0] > bestScore[0])
+				{
+					bestScore[0] = results[0];
+					bestMove[0] = position;
+				}
+			} else if (results[0] < bestScore[0])
 			{
-				board.place(position[0], position[1], 1, true);
-				bestScore = Double.min(bestScore, minimax(board, "Max"));
-				board.remove(position[0], position[1]);
+				bestScore[0] = results[0];
+				bestMove[0] = position;
 			}
-			return bestScore;
-		}
-		return 0.0;
+			board.remove(position[0], position[1]);
+		});
+		return new double[]{bestScore[0], bestMove[0][0], bestMove[0][1]};
 	}
 
 	public void makeMove(Board board)
 	{
-		int[][] testBoard = board.getBoard();
-		ArrayList<int[]> availablePosition = new ArrayList<int[]>();
-		for (int y = 0; y < 3; y++)
-			for (int x = 0; x < 3; x++)
-				if (testBoard[y][x] == 0)
-					availablePosition.add(new int[] {y, x});
-
-		int[] bestMove = null;
-		double bestScore = Double.NEGATIVE_INFINITY;
-		//Test out each available position, to get score.
-		for (int[] position : availablePosition)
-		{
-			board.place(position[0], position[1], 2, true);
-			double score = minimax(board, "Min");
-			System.out.println(Integer.toString(position[0]) + position[1] + score);
-			if (score > bestScore)
-			{
-				bestScore = score;
-				bestMove = position;
-			}
-			board.remove(position[0], position[1]);
-		}
-		board.place(bestMove[0], bestMove[1], 2);
+		double[] results = minimax(board, true);
+		board.place((int) results[1], (int) results[2], 2);
 	}
 
 }
